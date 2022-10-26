92f5738ee9d77d3c33d63e6b6fa8ea4016d8159b
Mari Yamaha <mari.yamaha.hwi@gmail.com>
2022-10-26 12:09:51 -0500

    clean up

--- src/lib/connection.ts
+++ src/lib/connection.ts
@@ -190,8 +190,6 @@ export class Connection {
 						// Added for backwards compatibility after Aug '22 interface changes
 						if (data.toApplication && !data.toApplication.componentState) {
 							data.toApplication.componentState = data.toApplication.eventHandlingCode;
-						} else if (!data.toApplication && !data.componentState) {
-							data.componentState = data.eventHandlingCode;
 						}
 						this.messages.next(data);
 					};
