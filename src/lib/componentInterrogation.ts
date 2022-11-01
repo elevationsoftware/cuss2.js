988e52ffcbbd7d3d5254de9dbf194bf48eb568c9
joncomstock <comstock2004@gmail.com>
2022-11-01 15:51:57 -0600

    Change isFaceReader to be more specific on Biometric lookup

--- src/lib/componentInterrogation.ts
+++ src/lib/componentInterrogation.ts
@@ -1,95 +1,101 @@
-import {CUSSDataTypes} from "./interfaces/cUSSDataTypes";
-import {MediaTypes} from "./interfaces/mediaTypes";
-import {ComponentTypes} from "./interfaces/componentTypes";
-import {ComponentCharacteristics} from "./interfaces/componentCharacteristics";
-import {EnvironmentComponent} from "./interfaces/environmentComponent";
-import {DeviceTypes} from "./interfaces/deviceTypes";
+import { CUSSDataTypes } from "./interfaces/cUSSDataTypes";
+import { MediaTypes } from "./interfaces/mediaTypes";
+import { ComponentTypes } from "./interfaces/componentTypes";
+import { ComponentCharacteristics } from "./interfaces/componentCharacteristics";
+import { EnvironmentComponent } from "./interfaces/environmentComponent";
+import { DeviceTypes } from "./interfaces/deviceTypes";
 
-const dsTypesHas = (charac0:ComponentCharacteristics, type: CUSSDataTypes) => {
+const dsTypesHas = (charac0: ComponentCharacteristics, type: CUSSDataTypes) => {
 	return charac0?.dsTypesList?.find((d) => d === type);
-}
-const mediaTypesHas = (mediaTypes:MediaTypes[], type: MediaTypes) => {
+};
+const mediaTypesHas = (mediaTypes: MediaTypes[], type: MediaTypes) => {
 	return mediaTypes?.find((m) => m === type);
-}
+};
 
 export class ComponentInterrogation {
-	static isAnnouncement = (component:EnvironmentComponent) => {
+	static isAnnouncement = (component: EnvironmentComponent) => {
 		return component.componentType === ComponentTypes.ANNOUNCEMENT;
-	}
+	};
 
-	static isFeeder = (component:EnvironmentComponent) => {
+	static isFeeder = (component: EnvironmentComponent) => {
 		return component.componentType === ComponentTypes.FEEDER;
-	}
+	};
 
-	static isDispenser = (component:EnvironmentComponent) => {
+	static isDispenser = (component: EnvironmentComponent) => {
 		return component.componentType === ComponentTypes.DISPENSER;
-	}
+	};
 
-	static isBagTagPrinter = (component:EnvironmentComponent) => {
+	static isBagTagPrinter = (component: EnvironmentComponent) => {
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
 		const mediaTypes = charac0.mediaTypesList;
 		return charac0.deviceTypesList?.[0] === DeviceTypes.PRINT && mediaTypesHas(mediaTypes, MediaTypes.BAGGAGETAG);
-	}
+	};
 
-	static isBoardingPassPrinter = (component:EnvironmentComponent) => {
+	static isBoardingPassPrinter = (component: EnvironmentComponent) => {
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
 		const mediaTypes = charac0.mediaTypesList;
 		return charac0.deviceTypesList?.[0] === DeviceTypes.PRINT && mediaTypesHas(mediaTypes, MediaTypes.BOARDINGPASS);
-	}
+	};
 
-	static isDocumentReader = (component:EnvironmentComponent) => {
+	static isDocumentReader = (component: EnvironmentComponent) => {
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
 		const mediaTypes = charac0.mediaTypesList;
 		return mediaTypesHas(mediaTypes, MediaTypes.PASSPORT);
-	}
+	};
 
-	static isBarcodeReader = (component:EnvironmentComponent) => {
+	static isBarcodeReader = (component: EnvironmentComponent) => {
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
 		return dsTypesHas(charac0, CUSSDataTypes.BARCODE);
-	}
+	};
 
-	static isCardReader = (component:EnvironmentComponent) => {
+	static isCardReader = (component: EnvironmentComponent) => {
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
 		const mediaTypes = charac0.mediaTypesList;
 		return mediaTypesHas(mediaTypes, MediaTypes.MAGCARD);
-	}
+	};
 
-	static isKeypad = (component:EnvironmentComponent) => {
+	static isKeypad = (component: EnvironmentComponent) => {
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
-		return dsTypesHas(charac0, CUSSDataTypes.KEY) && dsTypesHas(charac0, CUSSDataTypes.KEYUP) && dsTypesHas(charac0, CUSSDataTypes.KEYDOWN);
-	}
+		return (
+			dsTypesHas(charac0, CUSSDataTypes.KEY) &&
+			dsTypesHas(charac0, CUSSDataTypes.KEYUP) &&
+			dsTypesHas(charac0, CUSSDataTypes.KEYDOWN)
+		);
+	};
 
-	static isIllumination = (component:EnvironmentComponent) => {
+	static isIllumination = (component: EnvironmentComponent) => {
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
 		return charac0.deviceTypesList?.[0] === DeviceTypes.ILLUMINATION;
-	}
+	};
 
-	static isHeadset = (component:EnvironmentComponent) => {
+	static isHeadset = (component: EnvironmentComponent) => {
 		if (component.componentType !== ComponentTypes.MEDIAINPUT) return;
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
 		const mediaTypes = charac0.mediaTypesList;
 		return charac0.deviceTypesList?.[0] === DeviceTypes.ASSISTIVE && mediaTypesHas(mediaTypes, MediaTypes.AUDIO);
-	}
+	};
 
-	static isScale = (component:EnvironmentComponent) => {
+	static isScale = (component: EnvironmentComponent) => {
 		if (component.componentType !== ComponentTypes.DATAINPUT) return;
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
 		const mediaTypes = charac0.mediaTypesList;
 		return charac0.deviceTypesList?.[0] === DeviceTypes.SCALE && mediaTypesHas(mediaTypes, MediaTypes.BAGGAGE);
-	}
-	static isFaceReader = (component:EnvironmentComponent) => {
+	};
+
+	static isFaceReader = (component: EnvironmentComponent) => {
+		if (component.componentType !== ComponentTypes.DATAINPUT) return;
 		const charac0 = component.componentCharacteristics?.[0];
 		if (!charac0) return;
-		return dsTypesHas(charac0, CUSSDataTypes.BIOMETRIC);
-	}
-
+		const mediaTypes = charac0.mediaTypesList;
+		return charac0.deviceTypesList?.[0] === DeviceTypes.BIOMETRICS && mediaTypesHas(mediaTypes, MediaTypes.VISUAL);
+	};
 }
